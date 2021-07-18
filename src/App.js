5b7c9b1c605f3fab573e52b760115c7a447c6ca9
Magomed <mamergov01@bk.ru>
2021-07-18 11:38:00 +0300

    adress form is done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { commerce } from './lib/commerce'
-import { Products, Navbar, Cart } from './components'
+import { Products, Navbar, Cart, Checkout } from './components'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
@@ -66,6 +66,9 @@ const App = () => {
 						handleRemoveFromCart={handleRemoveFromCart}
 						handleEmptyCart={handleEmptyCart}
 					/>
+				</Route>
+				<Route path="/checkout" exact>
+					<Checkout cart={cart} />
 				</Route>
 			</Switch>
 		</Router>
---
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useFormContext, Controller } from 'react-hook-form'
+import { TextField, Grid } from '@material-ui/core'
+
+function FormInput({ name, label, register }) {
+	const { control } = useFormContext()
+	const isError = false
+
+	return (
+		<Grid item xs={12} sm={6}>
+			<Controller
+				control={control}
+				name={name}
+				render={({
+					field: { onChange, onBlur, value, name, ref },
+					formState,
+				}) => (
+					<TextField
+						onChange={onChange}
+						inputRef={ref}
+						label={label}
+						fullWidth
+						required
+						error={isError}
+					/>
+				)}
+			/>
+		</Grid>
+	)
+}
+
+export default FormInput
